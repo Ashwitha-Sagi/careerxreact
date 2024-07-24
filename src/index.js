a266d093f501900e8a2126eb38bdc12609601583
Ashwitha138 <ashwithasagi@gmail.com>
2024-07-24 21:04:24 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App19 from "./App19";
+import App37 from "./App37";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App19/>);
+root.render(<App37/>);
