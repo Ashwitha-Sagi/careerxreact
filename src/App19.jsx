5a067f255546becce1f795c13f1e3f9226219501
Ashwitha138 <ashwithasagi@gmail.com>
2024-07-24 21:01:46 +0530

    mmodified

--- src/App19.jsx
+++ src/App19.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import ReactDOM from "react-dom/client";
+import React from "react";
+import { useState, useEffect } from "react";
 export default function App19() {
-    const[runs,setRuns]=usestate(0);
-    const[wickets,setWickets]=useState(0);
+  const [runs, setRuns] = useState(0);
+  const [wickets, setWickets] = useState(0);
 
-    useEffect(()=>{
-        if(runs>0)console.log("Good Job");
-    },[runs]);
-    useEffect(()=>{
-        if(wickets>0)console.log("Better luck next time!");
-    },[wickets]);
+  useEffect(() => {
+    if (runs > 0) console.log("Good Job");
+  }, [runs]);
+  useEffect(() => {
+    if (wickets > 0) console.log("Better Luck next time!");
+  }, [wickets]);
 
   return (
     <div>
-        <button onClick={()=>setRuns(runs+1)}>Runs ({runs})</button>
-        <button onClick={()=>setWickets(wickets+1)}>Wickets ({wickets})</button>
+      <button onClick={() => setRuns(runs + 1)}>Runs ({runs})</button>
+      <button onClick={() => setWickets(wickets + 1)}>
+        Wickets ({wickets})
+      </button>
     </div>
-  )
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App37 from "./App37";
+import App19 from "./App19";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App37/>);
+root.render(<App19/>);
