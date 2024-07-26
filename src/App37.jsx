d5e4c99fab9b1756a290357c6cbab0214fef2487
Ashwitha138 <ashwithasagi@gmail.com>
2024-07-26 20:44:28 +0530

    modified App37

--- src/App37.jsx
+++ src/App37.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { useFetch } from "./useFetch";
-export default function App37() {
-  const url = "https://jsonplaceholder.typicode.com/posts";
-  const [data] = useFetch(url);
+import { useState, useEffect } from "react";
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    setMessage(`The count is ${count}`);
+  }, [count]);
+
   return (
     <div>
-      {data.map((element) => (
-        <div key={element.id}>{element.title}</div>
-      ))}
+      <h1>Counter: {count}</h1>
+      <p>{message}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
-}
+};
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App37 from "./App37";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App37/>);
